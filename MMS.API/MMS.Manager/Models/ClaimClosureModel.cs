395774d34870ea6a5901897a460d85f35ffab7e7
nasrinsultana014 <71586178+TSIProjects@users.noreply.github.com>
2021-02-09 02:01:29 +0600

    Initial codebase from tsiAILAB Git account.

---
+++ MMS.API/MMS.Manager/Models/ClaimClosureModel.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using Newtonsoft.Json;
+using System;
+
+namespace MMS.Manager
+{
+    public class ClaimClosureModel
+    {        
+        public int ClaimId { get; set; }
+        public string ClosureReason { get; set; }
+        [JsonConverter(typeof(DateConverter))]
+        public DateTime? ClosureDate { get; set; }
+        public string ClaimStatus { get; set; }
+        public ClaimNoteDto Note { get; set; }
+    }
+}
